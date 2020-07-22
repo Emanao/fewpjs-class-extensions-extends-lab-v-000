20973dbbc2da3956dc5de2030bfb32139a7a69d0
Eva Navarro <emanao@me.com>
2020-07-22 13:24:55 +0000

    Triangle class - Done

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ class Polygon{
     return this.sidesAry.reduce((acc, side)=>acc+=side);
   }
 }
+class Triangle extends Polygon{
+  get isValid(){
+    if (this.countSides===3){
+      return (this.sidesAry[0]+this.sidesAry[1]>this.sidesAry[2] &&
+      this.sidesAry[0]+this.sidesAry[2]>this.sidesAry[1]&&
+      this.sidesAry[1]+this.sidesAry[2]>this.sidesAry[0])
+    }
+  }
+}
