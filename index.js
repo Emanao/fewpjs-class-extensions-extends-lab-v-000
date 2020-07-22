16f022ccc25db6e776e5e73b192b167db2c2a86c
Eva Navarro <emanao@me.com>
2020-07-22 14:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,14 @@ class Triangle extends Polygon{
 class Square extends Polygon{
   get isValid(){
     if (this.countSides===4){
-       const sqLength = this.sidesAry[0];
-      console.log(this.sidesAry);
-      console.log(this.sidesAry.every(side=>side===sqLength));
+      const sqLength = this.sidesAry[0];
       return this.sidesAry.every(side=>side===sqLength)
+    }
+    
+  }
+  get area(){
+    if (this.countSides===4 && this.isValid){
+        return Math.pow(this.sidesAry[0],2);
     }
   }
 }
