a7788af3d1de2ba8f83a0b666292feee29a251de
Eva Navarro <emanao@me.com>
2020-07-22 13:08:28 +0000

    Polygon class - Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+class Polygon{
+  constructor(sidesAry){
+    this.sidesAry = sidesAry;
+
+  }
+  get countSides(){
+    return this.sidesAry.length;
+  }
+  get perimeter(){
+    return this.sidesAry.reduce((acc, side)=>acc+=side);
+  }
+}
